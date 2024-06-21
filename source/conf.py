3022fd64338b613de9a748d685ff8b55ca18b2f0
SabNK <sabnk@optictelecom.ru>
2024-06-21 15:04:59 +0300

    kroki ru

--- source/conf.py
+++ source/conf.py
@@ -1,5 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
+import sys
+import os
+import sphinx
+
 # -- Project information
 
 project = 'Kuwaiba'
@@ -41,3 +45,34 @@ gettext_uuid = True
 gettext_compact = 'docs'
 
 locale_dirs = ['../../locales']
+
+# Taken from Godot!
+# We want to host the localized images in godot-docs-l10n, but Sphinx does not provide
+# the necessary feature to do so. `figure_language_filename` has `{root}` and `{path}`,
+# but they resolve to (host) absolute paths, so we can't use them as is to access "../".
+# However, Python is glorious and lets us redefine Sphinx's internal method that handles
+# `figure_language_filename`, so we do our own post-processing to fix the absolute path
+# and point to the parallel folder structure in godot-docs-l10n.
+# Note: Sphinx's handling of `figure_language_filename` may change in the future, monitor
+# https://github.com/sphinx-doc/sphinx/issues/7768 to see what would be relevant for us.
+figure_language_filename = "{root}.{language}{ext}"
+
+cwd = os.getcwd()
+
+sphinx_original_get_image_filename_for_language = sphinx.util.i18n.get_image_filename_for_language
+
+
+def kuwaiba_get_image_filename_for_language(filename, env):
+    """
+    Hack the absolute path returned by Sphinx based on `figure_language_filename`
+    to insert our `../images` relative path to godot-docs-l10n's images folder,
+    which mirrors the folder structure of the docs repository.
+    The returned string should also be absolute so that `os.path.exists` can properly
+    resolve it when trying to concatenate with the original doc folder.
+    """
+    path = sphinx_original_get_image_filename_for_language(filename, env)
+    path = os.path.abspath(os.path.join("../../res/", os.path.relpath(path, cwd)))
+    return path
+
+sphinx.util.i18n.get_image_filename_for_language = kuwaiba_get_image_filename_for_language
+
--- source/start/contributing.rst
+++ source/start/contributing.rst
@@ -17,5 +17,10 @@ Translating
 
 Translation is organized with Weblate_. Typical process is shown on the diagram below.
 
+.. kroki:: 
+    :caption: Translating BPMN flow
+    :filename: ../res/translate.bpmn
+    :type: bpmn
+
 
 .. _Weblate: https://hosted.weblate.org/projects/kuwaiba/    
