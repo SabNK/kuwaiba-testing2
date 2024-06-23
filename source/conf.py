b43a37cb275c185d249c8fcc22512ed7ccc11760
SabNK <sabnk.optictelecom@gmail.com>
2024-06-23 15:15:00 +0300

    docs

--- source/conf.py
+++ source/conf.py
@@ -8,7 +8,6 @@ import sphinx
 on_rtd : bool = os.environ.get("READTHEDOCS", None) == "True"
 
 # get root project path - .
-
 cwd = os.getcwd()
 root = ""
 if on_rtd:
@@ -61,10 +60,10 @@ locale_dirs = ['../../locales']
 # Taken from Godot!
 # We want to host the localized images in kuwaiba_i18n, but Sphinx does not provide
 # the necessary feature to do so. `figure_language_filename` has `{root}` and `{path}`,
-# but they resolve to (host) absolute paths, so we can't use them as is to access "../".
+# but they resolve to (host) relative paths, so we can't use them as is to access "../".
 # However, Python is glorious and lets us redefine Sphinx's internal method that handles
-# `figure_language_filename`, so we do our own post-processing to fix the absolute path
-# and point to the parallel folder structure in godot-docs-l10n.
+# `figure_language_filename`, so we do our own post-processing to fix the path
+# and point to the parallel folder structure in kuwaiba_i18n.
 # Note: Sphinx's handling of `figure_language_filename` may change in the future, monitor
 # https://github.com/sphinx-doc/sphinx/issues/7768 to see what would be relevant for us.
 figure_language_filename = "{root}.{language}{ext}"
